605e01d4a5617273ff35d14b070c62743083b363
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-09-06 16:42:25 -0700

    Set up the server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("*", (req, res) => {
+  res.status(404);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});

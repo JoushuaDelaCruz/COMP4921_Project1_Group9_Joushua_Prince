8003fd02fd1c4b02a1ea9ef73ae8bc8215d4af40
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-09-18 20:34:06 -0700

    debug fullurl

--- server/routers/shortenURL.js
+++ server/routers/shortenURL.js
@@ -17,51 +17,19 @@ router.get("/", (req, res) => {
  res.render("shortenURL")
 });
 
-router.post('/shortenURL', (req, res) => {
+router.post('/', (req, res) => {
   console.log('Full URL submitted:');
 
-  const fullUrl = req.body.fullUrl; // Access the 'fullUrl' input from the form
+  const fullUrl = req.body.fullUrl; 
+  
+  
 
-  console.log('Full URL submitted:', fullUrl);
+  
 
-  // Process the 'fullUrl' as needed, such as URL shortening or storing it in a database
-  // For demonstration purposes, we'll just echo it back in this example
-  const shortUrl = 'http://shortened-url.com/' + generateShortCode(); // Replace with your logic
-
-  // You can store the 'fullUrl' and 'shortUrl' in a database or perform any other actions here
-
-  // Redirect to the root page and optionally pass the short URL
-  res.redirect(`/?shortUrl=${shortUrl}`);
 });
 
 
 
 
-// // Define a route for shortening URLs
-// router.post("/shortenURL", async (req, res) => {
-//   try {
-//     const { originalURL } = req.body;
-//     console.log(originalURL);
-
-//     // Generate a unique short ID using shortid
-//     const shortURL = shortId.generate();
-
-//     // You can store this shortURL in your database, associating it with the originalURL
-//     var results = await db_url.createURL({
-//       originalURL: originalURL,
-//       shortURL: shortURL,
-//     });
-//     console.log("Original URL:", originalURL);
-//     console.log("Short URL:", shortURL);
-
-//     if (results) {
-//       // Render the "shortenurl.ejs" template with the shortURL data
-//       res.render("shortenURL", { shortUrls: results }); // Pass the results or data you want to display
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// });
 
 module.exports = router;

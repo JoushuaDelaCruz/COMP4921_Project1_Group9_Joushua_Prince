f27bd3364c9dbad5d552047e337c8d80e4e41fec
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-09-18 18:50:35 -0700

    Created homepage

--- server/routers/homepage.js
+++ server/routers/homepage.js
@@ -4,9 +4,36 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  const { shortener, text } = req.query;
   const authenticated = req.session ? req.session.authenticated : false;
-  console.log(authenticated);
-  res.render("index", { isUserSignedIn: authenticated });
+  if (shortener) {
+    const bundle = {
+      isUserSignedIn: authenticated,
+      imageClass: "text-light",
+      shortenerClass: "active",
+      textClass: "text-light",
+    };
+    res.render("index", bundle);
+    return;
+  }
+  if (text) {
+    const bundle = {
+      isUserSignedIn: authenticated,
+      imageClass: "text-light",
+      shortenerClass: "text-light",
+      textClass: "active",
+    };
+    res.render("index", bundle);
+    return;
+  }
+  const bundle = {
+    isUserSignedIn: authenticated,
+    imageClass: "active",
+    shortenerClass: "text-light",
+    textClass: "text-light",
+  };
+  res.render("index", bundle);
+  return;
 });
 
 router.post("/user", async (req, res) => {

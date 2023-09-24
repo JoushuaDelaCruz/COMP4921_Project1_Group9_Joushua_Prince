c9813c2aa058ecea87fe71b796d70fd1520f69bc
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-09-24 15:06:06 -0700

    Changed session on top of the routers

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-const express = require("express");
 require("./utils");
+require("dotenv").config();
+const express = require("express");
 const MongoStore = require("connect-mongo");
 const session = require("express-session");
 const bodyParser = require("body-parser");
@@ -11,20 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const imageRouter = require("./routers/imageUrl");
-const signUpRouter = require("./routers/signUp");
-const shortenURLrouter = require("./routers/shortenURL");
-const homeRouter = require("./routers/homepage");
-const db_users = include("database/users");
-
-// app.use("/login", loginRouter);
-app.use("/signup", signUpRouter);
-app.use("/shortenURL", shortenURLrouter);
-app.use("/imageUrls", imageRouter);
-app.use("/home", homeRouter);
-
 const bcrypt = require("bcrypt");
-const saltRounds = 12;
 const expireTime = 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
 
 //** MongoDB Session */
@@ -50,10 +38,22 @@ app.use(
     saveUninitialized: false,
     resave: true,
     cookie: {
-      maxAge: 1000 * 60 * 60, // 1 hour
+      maxAge: expireTime, // 1 hour
     },
   })
 );
+
+const imageRouter = require("./routers/imageUrl");
+const signUpRouter = require("./routers/signUp");
+const shortenURLrouter = require("./routers/shortenURL");
+const homeRouter = require("./routers/homepage");
+const db_users = include("database/users");
+
+// app.use("/login", loginRouter);
+app.use("/signup", signUpRouter);
+app.use("/shortenURL", shortenURLrouter);
+app.use("/imageUrls", imageRouter);
+app.use("/home", homeRouter);
 
 app.get("/login", (req, res) => {
   const invalid = req.query.invalid;
@@ -71,10 +71,6 @@ app.post("/login/user", async (req, res) => {
   var results = await db_users.getUser({ user: user });
 
   if (results) {
-    console.log(results[0]);
-  }
-
-  if (results) {
     if (results.length === 1) {
       // Ensure there is exactly one matching user
       const storedHashedPassword = results[0].password;
@@ -82,10 +78,9 @@ app.post("/login/user", async (req, res) => {
       // Compare the user-entered password with the stored hashed password
       if (bcrypt.compareSync(password, storedHashedPassword)) {
         req.session.authenticated = true;
-        req.session.user = user;
+        req.session.user = results[0].username;
         req.session.user_id = results[0].user_id;
         req.session.cookie.maxAge = expireTime;
-        console.log("Logging in: " + results[0].user_id);
         res.redirect("/home");
         return;
         // Handle the login success case here

c64da286618f9c1861bc0a69434e9b1935ebac9d
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-09-26 16:39:14 -0700

    Imported urlInfo

--- server/database/db_imageUrls.js
+++ server/database/db_imageUrls.js
@@ -1,29 +1,8 @@
 const database = include("mySQLDatabaseConnection");
-
-const insertUrlInfoAndGetUrlInfoId = async () => {
-  const newUrl = `
-    INSERT INTO urls_info (date_created) VALUES (:date_created);
-  `;
-
-  const params = {
-    date_created: new Date(),
-  };
-
-  try {
-    const results = await database.query(newUrl, params);
-
-    console.log("Successfully created an url_info");
-    console.log(results[0]);
-    return results[0].insertId;
-  } catch (err) {
-    console.log("Error inserting new row for url_info");
-    console.log(err);
-    return false;
-  }
-};
+const urlInfo = require("./db_urls_info");
 
 const uploadImage = async (data) => {
-  const urlInfoFk = await insertUrlInfoAndGetUrlInfoId();
+  const urlInfoFk = await urlInfo.insertUrlInfoAndGetUrlInfoId();
   const uploadImageSQL = `
   	INSERT INTO ImageUrls
   	(image_id, uploader_id, cloudinary_public_id, url_info_id)

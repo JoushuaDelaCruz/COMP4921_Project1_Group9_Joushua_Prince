403f566e7bc67d8d5ad8dc62efecf3fb6591f887
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-09-28 16:19:05 -0700

    Moved the urls_info related statements

--- server/database/db_urls_info.js
+++ server/database/db_urls_info.js
@@ -21,3 +21,49 @@ exports.insertUrlInfoAndGetUrlInfoId = async () => {
     return false;
   }
 };
+
+const deactivateUrl = async (url_info_id) => {
+  const deactivateSQL = `
+  UPDATE urls_info 
+  SET is_active = 0
+  WHERE url_info_id = :url_info_id;`;
+
+  const params = {
+    url_info_id: url_info_id,
+  };
+
+  try {
+    const results = await database.query(deactivateSQL, params);
+    console.log("Successfully deactivated image");
+    console.log(results[0]);
+    return true;
+  } catch (err) {
+    console.log("Error failed to deactivate image");
+    console.log(err);
+    return false;
+  }
+};
+
+const activateUrl = async (url_info_id) => {
+  const activateSQL = `
+  UPDATE urls_info 
+  SET is_active = 1
+  WHERE url_info_id = :url_info_id;`;
+
+  const params = {
+    url_info_id: url_info_id,
+  };
+
+  try {
+    const results = await database.query(activateSQL, params);
+    console.log("Successfully activated image");
+    console.log(results[0]);
+    return true;
+  } catch (err) {
+    console.log("Error failed to activate image");
+    console.log(err);
+    return false;
+  }
+};
+
+module.exports = { activateUrl, deactivateUrl };

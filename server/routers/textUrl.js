db5880be24e27a9ce9d482406fa6435e8d6e9139
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-09-28 22:26:10 -0700

    Added an upload route

--- server/routers/textUrl.js
+++ server/routers/textUrl.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const db_textUrl = include("database/db_imageUrls");
+const db_textUrl = include("database/db_textUrls");
 const db_urlInfo = include("database/db_urls_info");
 const shortId = require("shortid");
 
 router.post("/upload", async (req, res) => {
-  const { text, title } = req.body;
+  const text = req.body.text;
+  const title = req.body.title || "Untitled";
+  const text_id = shortId.generate();
+  const uploader_id = req.session.user_id;
+  const textData = {
+    uploader_id: uploader_id,
+    text_id: text_id,
+    text: text,
+    title: title,
+  };
+  await db_textUrl.uploadText(textData);
   res.redirect("/home?text=true");
   return;
 });
+
+module.exports = router;

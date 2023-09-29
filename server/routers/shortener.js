c696b7c055376f7f9da893357142871a44d17306
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-09-28 21:38:15 -0700

    Remove dead code

--- server/routers/shortener.js
+++ server/routers/shortener.js
@@ -27,15 +27,11 @@ router.post("/", async (req, res) => {
   }
 
   else {
-
  // Generate a unique short ID using shortid
  const shortcode = shortId.generate();
-
  // Ensure that the short URL starts with "http://"
  const shortURL = shortcode;
- // console.log("printing URL" + shortURL);
-
-
+ 
  var results = await db_shortener.createURL({
    originalURL: fullUrl,
    shortURL: shortURL,
@@ -47,40 +43,6 @@ router.post("/", async (req, res) => {
  }  }
 });
 
-
-router.get("/", async (req, res) => {
-  const shortcode = req.body.short_code;
-
-  // Look up the original URL associated with the shortcode in your database
-  const originalURL = await db_shortener.getOriginalURL(shortcode);
-
-  if (originalURL) {
-    // Redirect to the original URL
-    // increment url click inside urls 
-    // render number of clicks to table in the page
-    router.get("/:shortcode", async (req, res) => {
-  const shortcode = req.params.shortcode;
-
-  // Look up the original URL associated with the shortcode in your database
-  const originalURL = await db_shortener.getOriginalURL(shortcode);
-
-
-  if (originalURL) {
-    // Redirect to the original URL
-    // increment url click inside urls
-    // render number of clicks to table in the page
-    res.redirect(originalURL);
-  } else {
-    // Handle the case where the shortcode doesn't exist
-    res.status(404).send("Short URL not found");
-  }
-});
-    res.redirect(originalURL);
-  } else {
-    // Handle the case where the shortcode doesn't exist
-    res.status(404).send("Short URL not found");
-  }
-});
 
 
 router.get("/:shortcode", async (req, res) => {

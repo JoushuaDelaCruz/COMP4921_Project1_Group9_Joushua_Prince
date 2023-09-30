5a82ad47482eb77d0698ced67812410619e290f0
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-09-30 15:13:11 -0700

    Changed port to 5000

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const MongoStore = require("connect-mongo");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.set("view engine", "ejs");

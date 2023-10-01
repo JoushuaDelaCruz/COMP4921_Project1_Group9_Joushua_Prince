06bb88ac4d413dd1c1713f56219d4fa2014ef948
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-01 14:31:49 -0700

    Uncommented the return of results

--- server/database/db_imageUrls.js
+++ server/database/db_imageUrls.js
@@ -33,27 +33,27 @@ const uploadImage = async (data) => {
 
 const getUploadedImages = async () => {
   const imagesSQL = `
-    SELECT 
-      image_id, 
-      cloudinary_public_id, 
-      date_created, 
-      uploader_id, 
-      username, 
-      num_hits, 
-      last_date_visited, 
-      is_active, 
-      ui.url_info_id
-    FROM image_url as image
-    LEFT JOIN user on uploader_id = user_id
-    LEFT JOIN urls_info as ui on ui.url_info_id = image.url_info_id
+  SELECT 
+    image_id, 
+    cloudinary_public_id, 
+    date_created, 
+    uploader_id, 
+    username, 
+    num_hits, 
+    last_date_visited, 
+    is_active, 
+    url_info_id
+  FROM image_url as image
+  JOIN user on uploader_id = user_id
+  JOIN urls_info using (url_info_id)
 	`;
 
   try {
     const results = await database.query(imagesSQL);
 
-    // console.log("Successfully retrieves all uploaded images");
-    // console.log(results[0]);
-    // return results[0];
+    console.log("Successfully retrieves all uploaded images");
+    console.log(results[0]);
+    return results[0];
   } catch (err) {
     console.log("Error failed to retrieve uploaded images");
     console.log(err);

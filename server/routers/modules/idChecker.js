70ed7d49d8e9766c50ae1d480486e65c7c47d147
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-03 20:37:16 -0700

    Added an customizer name checker

--- server/routers/modules/idChecker.js
+++ server/routers/modules/idChecker.js
@@ -2,8 +2,9 @@ const checkLength = (name) => {
   return name.length > 30;
 };
 
-const checkDb = (db_function, name) => {
-  return db_function(name);
+const checkNameExistsInDB = async (db_function, name) => {
+  const isExists = await db_function(name);
+  return isExists;
 };
 
 const CheckSpecialChar = (name) => {
@@ -11,15 +12,15 @@ const CheckSpecialChar = (name) => {
   return specialCharsAndSpace.test(name);
 };
 
-exports.checkName = (db_function, name) => {
+exports.checkName = async (db_function, name) => {
   if (checkLength(name)) {
     return `The ${name} is too long, please choose another name.`;
   }
-  if (checkDb(db_function, name)) {
+  if (await checkNameExistsInDB(db_function, name)) {
     return `${name} already exists. Please choose another name.`;
   }
   if (CheckSpecialChar(name)) {
     return `The '${name}' cannot contain special characters and space.`;
   }
-  return null;
+  return "";
 };
